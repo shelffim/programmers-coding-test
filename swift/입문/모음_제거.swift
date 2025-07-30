224c3cf17eb9db1d7500d9a9db215f1f1d6a172c
shelffim <shelffim0219@gmail.com>
2025-07-30 17:39:48 +0900

    모음 제거

--- swift/입문/모음_제거.swift
+++ swift/입문/모음_제거.swift
@@ -1,6 +1,19 @@
 import Foundation
 
 func solution(_ my_string:String) -> String {
-    let filter_my_string = my_string.filter { ["a","e","i","o","u"].contains($0) == false }
-    return filter_my_string
+    guard (1...1000).contains(my_string.count),
+      my_string.allSatisfy({ ($0 >= "a" && $0 <= "z") || $0 == " " }) else { return "" }
+    return my_string.filter { !["a","e","i","o","u"].contains($0) }
 }
+
+// import Foundation
+
+// func solution(_ my_string:String) -> String {
+//     guard (1...1000).contains(my_string.count),
+//       my_string.allSatisfy({ ($0 >= "a" && $0 <= "z") || $0 == " " }) else {
+//     return ""
+// }
+//     return Array(my_string).filter { !["a","e","i","o","u"].contains($0) }.map { String($0) }.joined(separator: "")
+// }
+
+// filter 반환, swift는 in 없음(파이썬이랑 헷갈림)
