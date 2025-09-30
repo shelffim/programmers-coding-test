c621910531f1a320ebe565f73b03341b83967cb4
shelffim <shelffim0219@gmail.com>
2025-09-30 20:16:16 +0900

    자바 - 문자 개수 세기 다시 풀어보기

--- java/기초/문자_개수_세기.java
+++ java/기초/문자_개수_세기.java
@@ -1,23 +1,12 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 class Solution {
     public int[] solution(String my_string) {
-        String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        Map<Character, Integer> alphaMap = new HashMap<>();
-        for(char c: alpha.toCharArray()) {
-            alphaMap.put(c,0);
-        }
+        int[] answer = new int[52];
         for(char c: my_string.toCharArray()) {
-            int count = alphaMap.get(c);
-            alphaMap.put(c, count + 1);
-        }
-        List<Integer> count_list = new ArrayList<>(alphaMap.values());
-        int[] answer = new int[count_list.size()];
-        for (int i = 0; i < count_list.size(); i++) {
-            answer[i] = count_list.get(i);
+            if ('A' <= c && c <= 'Z') {
+                answer[c - 'A']++;
+            } else {
+                answer[c - 'a' + 26]++;
+            }
         }
         return answer;
     }
