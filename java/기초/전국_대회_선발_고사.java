26c0e9adf396a3e362073117e931f8d1760ff6d4
shelffim <shelffim0219@gmail.com>
2025-10-15 12:09:20 +0900

    자바 - 전국 대회 선발 고사 객체 사용

--- java/기초/전국_대회_선발_고사.java
+++ java/기초/전국_대회_선발_고사.java
@@ -1,62 +1,38 @@
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        ArrayList<Student> list = new ArrayList<>();
         for(int i = 0; i < rank.length; i++) {
             if(attendance[i]) {
-                map.put(rank[i], i);
+                list.add(new Student(rank[i], i));
             }
         }
-        int[] answer = new int[3];
-        int idx = 0;
-        for(Integer key: map.keySet()) {
-            if (idx >= 3) {
-                break;
-            }
-            answer[idx++] = map.get(key);
+        Collections.sort(list);
+        return 10000 * list.get(0).getIdx() + 100 * list.get(1).getIdx() + list.get(2).getIdx();
+    }
+    
+    class Student implements Comparable<Student> {
+        private int grade;
+        private int idx;
+
+        public Student(int grade, int idx) {
+            this.grade = grade;
+            this.idx = idx;
         }
-        return 10000 * answer[0] + 100 * answer[1] + answer[2];
+
+        public int getGrade() {
+            return grade;
+        }
+
+        public int getIdx() {
+            return idx;
+        }
+
+        @Override
+        public int compareTo(Student other) {
+            return this.grade - other.grade;
+        }
     }
 }
-
-/*
-TreeMap에서 바로 값을 꺼내서 학생 번호를 가져오는 다양한 방법들:
-
-1. 현재 코드 (올바른 방법)
-   for(Integer key: map.keySet()) {
-       answer[idx++] = map.get(key);
-   }
-
-2. Iterator 사용
-   Iterator<Integer> iterator = map.keySet().iterator();
-   for(int i = 0; i < 3; i++) {
-       if(iterator.hasNext()) {
-           answer[i] = map.get(iterator.next());
-       }
-   }
-
-3. EntrySet 사용
-   int idx = 0;
-   for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-       if(idx >= 3) break;
-       answer[idx++] = entry.getValue();
-   }
-
-4. Stream 사용 (Java 8+)
-   int[] selectedStudents = map.entrySet().stream()
-       .limit(3)
-       .mapToInt(Map.Entry::getValue)
-       .toArray();
-
-5. 가장 간결한 방법 - firstKey() 사용
-   int a = map.get(map.firstKey());
-   int b = map.get(map.higherKey(map.firstKey()));
-   int c = map.get(map.higherKey(map.higherKey(map.firstKey())));
-   return 10000 * a + 100 * b + c;
-
-TreeMap 메서드 설명:
-- firstKey(): 가장 작은 키(1등)를 반환
-- higherKey(key): 주어진 키보다 큰 키 중 가장 작은 키를 반환
-- get(key): 키에 해당하는 값(학생 번호)을 반환
-*/
