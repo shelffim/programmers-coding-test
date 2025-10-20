c8551afac1d7a79017bb7481f4d81fa3e9f8612d
shelffim <shelffim0219@gmail.com>
2025-10-20 22:37:11 +0900

    feat: [Java] [Lv1] 자연수 뒤집어 배열로 만들기 - 수학/구현
    - 문제: 자연수를 뒤집어 배열에 저장하는 문제
    - 해결: 10으로 나눈 나머지로 일의 자리 추출, 몫으로 다음 자릿수로 이동 후 리스트에 저장하고 리스트에서 배열로 저장
    - 복잡도: 시간 O(N)
    - 추가: README 작성 (문제 설명, 해결 과정, 오답 노트)

--- src/reverse_digits_array/Solution.java
+++ src/reverse_digits_array/Solution.java
@@ -1,4 +1,38 @@
 package reverse_digits_array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
+    public int[] solution(long n) {
+        if (n == 0) {
+            return new int[] {0};
+        }
+
+        List<Integer> list = getList(n);
+
+        return getArray(list);
+    }
+
+    private int[] getArray(List<Integer> list) {
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+    private List<Integer> getList(long n) {
+        List<Integer> list = new ArrayList<>();
+        while (n > 0) {
+            list.add((int) (n % 10));
+            n /= 10;
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution(12345);
+    }
 }
