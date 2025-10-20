74c9f88dc1b325cbcc66b47f825a617f30d9f950
shelffim <shelffim0219@gmail.com>
2025-10-20 23:02:57 +0900

    refactor: [Java][Lv1] 자연수 뒤집어 배열로 만들기 - StringBuilder 기반으로 재구현
    - 변경: 수학적 나머지/몫 반복 대신 StringBuilder(reverse) + 문자→정수 변환으로 단순화
    - 이유: 가독성 향상, 변환 로직 명확화
    - 복잡도: 시간 O(N), 공간 O(N)
    - 검증: n=12345, n=0, Long 범위 값 확인
    - 문서: README에 대안 접근 및 비교 추가

--- src/reverse_digits_array/Solution.java
+++ src/reverse_digits_array/Solution.java
@@ -1,35 +1,17 @@
 package reverse_digits_array;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
     public int[] solution(long n) {
-        if (n == 0) {
-            return new int[] {0};
+        StringBuilder sb = new StringBuilder(String.valueOf(n)).reverse();
+
+        int[] answer = new int[sb.length()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = Integer.parseInt(sb.charAt(i) + "");
         }
 
-        List<Integer> list = getList(n);
-
-        return getArray(list);
-    }
-
-    private int[] getArray(List<Integer> list) {
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
         return answer;
     }
 
-    private List<Integer> getList(long n) {
-        List<Integer> list = new ArrayList<>();
-        while (n > 0) {
-            list.add((int) (n % 10));
-            n /= 10;
-        }
-        return list;
-    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
