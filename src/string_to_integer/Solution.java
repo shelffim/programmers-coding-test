b0c8dc8c7e7e08601e600d7de662c28c77df1291
shelffim <shelffim0219@gmail.com>
2025-10-25 16:13:36 +0900

    refactor: [Java][Lv1]  - 문자열을 정수로 바꾸기  - 수학/구현
    - 변경: 불필요한 replaceAll 제거
    - 이유: 이미 부호 처리 로직이 내장되어 있음. 가독성 향상
    - 복잡도: 시간 O(N)
    - 추가: README 작성 (문제 설명, 해결 과정, 오답 노트)

--- src/string_to_integer/Solution.java
+++ src/string_to_integer/Solution.java
@@ -2,19 +2,11 @@ package string_to_integer;
 
 public class Solution {
     public int solution(String s) {
-        int answer;
-        String digit = "";
-        if (s.charAt(0) == '+' || s.charAt(0) == '-') {
-            digit = s.charAt(0) + "";
-            answer = Integer.parseInt(s.replaceAll("[^0-9]",""));
-        } else {
-            answer = Integer.parseInt(s);
-        }
-        return digit.equals("-") ? -1 * answer : answer;
+        return Integer.parseInt(s);
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.solution("-1234");
+        System.out.println("solution.solution(\"-1234\") = " + solution.solution("-1234"));
     }
 }
