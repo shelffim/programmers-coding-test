540202ee36a5dc12b50abd3978087a2d6e45f455
shelffim <shelffim0219@gmail.com>
2025-10-28 14:28:24 +0900

    refactor: [Java][Lv1]  - 문자열 내 p와 y의 개수  - 수학/구현
    - 변경: String 객체의 chars()를 사용하여 stream으로 변환 후, filter를 사용
    - 이유: 가독성 향상, 로직 명확화
    - 복잡도: 시간 O(N)
    - 추가: README 작성 (문제 설명, 해결 과정, 오답 노트)

--- src/p_and_y_count_in_string/Solution.java
+++ src/p_and_y_count_in_string/Solution.java
@@ -2,20 +2,12 @@ package p_and_y_count_in_string;
 
 public class Solution {
     boolean solution(String s) {
-        int pCount = 0;
-        int yCount = 0;
-        for (char c: s.toLowerCase().toCharArray()) {
-            if (c == 'p') {
-                pCount++;
-            } else if (c == 'y') {
-                yCount++;
-            }
-        }
-        return pCount == yCount;
+        String lowerS = s.toLowerCase();
+        return lowerS.chars().filter(c -> c == 'y').count() == lowerS.chars().filter(c -> c == 'p').count();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.solution("pPoooyY");
+        solution.solution("pPoooyYy");
     }
 }
