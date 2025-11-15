10fbfa367ea1d6887f8a36e0f05319bfeaa02f43
shelffim <shelffim0219@gmail.com>
2025-11-15 12:55:13 +0900

    refactor: [Java][Lv1]  - 핸드폰 번호 가리기  - 수학/구현
    - 변경: 입력 받은 문자열의 길이 -4 부터 문자열의 길이까지를 반복문으로 저장했지만 substring으로 변경
    - 이유: 가독성 향상, 코드 라인 수 감소
    - 복잡도: 시간 O(N)
    - 추가: README 작성 (문제 설명, 해결 과정, 오답 노트)

--- src/hide_phone_number/Solution.java
+++ src/hide_phone_number/Solution.java
@@ -4,9 +4,8 @@ public class Solution {
     public String solution(String phone_number) {
         int phone_number_length = phone_number.length();
         StringBuilder sb = new StringBuilder();
-        sb.append("*".repeat(phone_number_length - 4));
-        for (int i = 4; i > 0; i--)
-            sb.append(phone_number.charAt(phone_number_length - i));
+        sb.append("*".repeat(phone_number_length - 4))
+                .append(phone_number.substring(phone_number_length - 4, phone_number_length));
         return sb.toString();
     }
 
